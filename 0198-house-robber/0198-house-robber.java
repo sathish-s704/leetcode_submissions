8475c8ca92cb50ccfd7b0ed431c9e60814808427
SATHISH S <sathishsathish75363@gmail.com>
2026-01-05 19:27:33 +0530

    Time: 0 ms (100%), Space: 42.9 MB (17.32%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n=nums.length;
+        if(n==1)return nums[0];
+        int dp[]=new int[n+1];
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<n+1;i++){
+            int steal=nums[i-1]+dp[i-2];
+            int skip=dp[i-1];
+            dp[i]=Math.max(steal,skip);
+        }
+        return dp[n];
+        
+    }
+}
